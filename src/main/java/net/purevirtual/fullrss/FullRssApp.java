e5a03d4fbc9cedc0b48a1751ff1087eb78b1d2c1
Lukasz Stosik <lstosik@gmail.com>
2010-02-08 02:00:19 +0100

    Wicket configuration type is now based on server info string.

--- src/main/java/net/purevirtual/fullrss/FullRssApp.java
+++ src/main/java/net/purevirtual/fullrss/FullRssApp.java
@@ -1,29 +1,45 @@
 package net.purevirtual.fullrss;
+
+import java.util.Enumeration;
 import net.purevirtual.fullrss.pages.Hello;
 import org.apache.wicket.protocol.http.HttpSessionStore;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.session.ISessionStore;
+
 public class FullRssApp extends WebApplication {
-    public FullRssApp() {
-		
-    }
 
-    /**
-     * @see org.apache.wicket.Application#getHomePage()
-     */
+	public FullRssApp() {
+	}
+
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
 	@Override
-    public Class getHomePage() {
-        return Hello.class;
-    }
-	    @Override
-    protected void init() {
-      super.init();
-      getResourceSettings().setResourcePollFrequency(null);
-    }
+	public Class getHomePage() {
+		return Hello.class;
+	}
 
-    @Override
-    protected ISessionStore newSessionStore() {
-      return new HttpSessionStore(this);
-    }
+	@Override
+	protected void init() {
+		super.init();
+		if (this.getServletContext().getServerInfo().contains("Development")) {
+			System.setProperty("wicket.configuration", DEVELOPMENT);
+		} else {
+			System.setProperty("wicket.configuration", DEPLOYMENT);
+		}
+		getResourceSettings().setResourcePollFrequency(null);
+	}
 
+	@Override
+	protected ISessionStore newSessionStore() {
+		return new HttpSessionStore(this);
+	}
+
+	private void printSystemProperties() {
+		Enumeration<String> attributeNames = this.getServletContext().getAttributeNames();
+		while (attributeNames.hasMoreElements()) {
+			String attributeName = attributeNames.nextElement();
+			System.err.println(attributeName + " = " + this.getServletContext().getAttribute(attributeName));
+		}
+	}
 }
